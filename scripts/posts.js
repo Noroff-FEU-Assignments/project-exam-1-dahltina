8c5624033379465573104a3e0447c3410ce90712
dahltina <71266815+dahltina@users.noreply.github.com>
2021-04-29 11:45:00 +0200

    blog page layout and api call

--- scripts/posts.js
+++ scripts/posts.js
@@ -24,16 +24,56 @@ function displayPosts(post) {
     console.log(post);
     for (let i = 0; i < post.length; i++) {
 
-        blogContainer.innerHTML += `<div class="blog-page-box one">
-                                    <img src="${post[i]._embedded['wp:featuredmedia']['0'].source_url}"  class="box-image box-image-one">
-                                    <div class="box-text box-text-one">
-                                        <h3 class="carousel-text post-h3">${post[i].title.rendered}</h3>
-                                        <p class="date">${post[i].date} - Travel - USA</p>
-                                        <p class="excerpt">${post[i].excerpt.rendered}</p>
-                                    </div>
-                                </div>`
+        blogContainer.innerHTML += `<div class="blog-page-box">
+                                        <img src="${post[i]._embedded['wp:featuredmedia']['0'].source_url}"  class="box-image box-image-one">
+                                        <div class="box-text box-text-one">
+                                            <h3 class="carousel-text post-h3">${post[i].title.rendered}</h3>
+                                            <p class="date">${post[i].date} - Travel - USA</p>
+                                            <p class="excerpt">${post[i].excerpt.rendered}</p>
+                                        </div>
+                                    </div>`
     }
 }
+
+
+// get featured post
+const featuredPostUrl = "https://nomadlife.tinadahl.no/wp-json/wp/v2/posts/50/?_embed";
+
+async function getFeaturedPost() {
+
+    try {
+        const response = await fetch(featuredPostUrl);
+        const results = await response.json();
+        displayFeaturedPost(results);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+getFeaturedPost()
+
+const featuredContainer = document.querySelector(".featured-post-container")
+
+function displayFeaturedPost(post) {
+    console.log(post);
+
+    featuredContainer.innerHTML += `<img src="${post._embedded['wp:featuredmedia']['0'].source_url}"  class="carousel-image">
+                                    <div class="overlay">
+                                        <h3 class="carousel-text">${post.title.rendered}</h3>
+                                        <p>${post.excerpt.rendered}</p>
+                                    </div>
+                                </div>`
+}
+
+
+// div class="featured-post-container">
+//                 <img class="featured-post-image" src="images/kerala-3.jpg">
+//                 <div class="overlay">
+//                     <h3 class="carousel-text"></h3>
+//                     <p></p>
+//                 </div>
+//             </div>
 
 
 // function displayPosts(post) {
